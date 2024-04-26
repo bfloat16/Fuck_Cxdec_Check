33d42ab381e2e26762c1f92576f79f689dd17a5d
bfloat16 <a1004428886@126.com>
2024-04-27 02:32:47 +0800

    update

--- Version/dllmain.cpp
+++ Version/dllmain.cpp
@@ -1,10 +1,11 @@
-#include "Hijack.h"
+﻿#include "Hijack.h"
 #include <detours.h>
 #include <wchar.h>
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 typedef HMODULE(WINAPI* pLoadLibraryExW)(
     LPCWSTR lpLibFileName,
@@ -13,35 +14,77 @@ typedef HMODULE(WINAPI* pLoadLibraryExW)(
     );
 pLoadLibraryExW rawLoadLibraryExW;
 
-void ModifyDll(const std::wstring& path) {
-    std::fstream dllFile;
-    dllFile.open(path, std::ios::in | std::ios::out | std::ios::binary);
-    if (dllFile.is_open()) {
-        dllFile.seekp(0x0000B7A0, std::ios::beg);
-        char newBytes[] = {
-            0x55,                          // push ebp
-            0x8B, 0xEC,                    // mov ebp, esp
-            0x8B, 0x4D, 0x08,              // mov ecx, dword ptr ss:[ebp+0x08]
-            0x85, 0xC9,					   // test ecx, ecx
-            0xEB, 0x13, 				   // jmp short 0x13 <===== Force Jump
-            0xFF, 0x75, 0x10,			   // push dword ptr ss:[ebp+0x10]
-            0xFF, 0x75, 0x0C,			   // push dword ptr ss:[ebp+0x0C]
-            0xE8, 0x3B, 0x0F, 0x00, 0x00,  // call 0x0000C0F0
-            0x84, 0xC0,					   // test al, al
-            0x74, 0x04,					   // je short 0x04
-            0xB0, 0x01,					   // mov al, 0x01
-            0x5D,						   // pop ebp
-            0xC3,						   // ret
-            0xB0, 0x01,					   // mov al, 0x01   <===== Force bypass
-            0x5D,						   // pop ebp
-            0xC3 						   // ret
-        };
-        dllFile.write(newBytes, sizeof(newBytes));
-        dllFile.close();
+static void ModifyDll(const std::wstring& path) {
+    std::fstream dllFile(path, std::ios::in | std::ios::out | std::ios::binary);
+    if (!dllFile.is_open()) {
+        std::wcerr << L"无法打开文件: " << path << std::endl;
+        return;
     }
+
+    dllFile.seekg(0, std::ios::end);
+    size_t size = dllFile.tellg();
+    dllFile.seekg(0, std::ios::beg);
+    char* buffer = new char[size];
+    dllFile.read(buffer, size);
+
+    char searchBytes[] = {
+        0x55,                         // push ebp
+        0x8B, 0xEC,                   // mov ebp, esp
+        0x8B, 0x4D, 0x08,             // mov ecx, dword ptr ss:[ebp+0x08]
+        0x85, 0xC9,                   // test ecx, ecx
+        0x74, 0x13,                   // je short 0x13
+        0xFF, 0x75, 0x10,             // push dword ptr ss:[ebp+0x10]
+        0xFF, 0x75, 0x0C,             // push dword ptr ss:[ebp+0x0C]
+        0xE8, 0xFB, 0x08, 0x00, 0x00, // call 0x0000C0F0
+        0x84, 0xC0,                   // test al, al
+        0x74, 0x04,                   // je short 0x04
+        0xB0, 0x01,                   // mov al, 0x01
+        0x5D,                         // pop ebp
+        0xC3,                         // ret
+        0x32, 0xC0,                   // xor al, al
+        0x5D,                         // pop ebp
+        0xC3                          // ret
+    };
+
+    size_t searchSize = sizeof(searchBytes) / sizeof(char);
+
+    char newBytes[] = {
+        0x55,                          // push ebp
+        0x8B, 0xEC,                    // mov ebp, esp
+        0x8B, 0x4D, 0x08,              // mov ecx, dword ptr ss:[ebp+0x08]
+        0x85, 0xC9,					   // test ecx, ecx
+        0xEB, 0x13, 				   // jmp short 0x13 <===== Force Jump
+        0xFF, 0x75, 0x10,			   // push dword ptr ss:[ebp+0x10]
+        0xFF, 0x75, 0x0C,			   // push dword ptr ss:[ebp+0x0C]
+        0xE8, 0x3B, 0x0F, 0x00, 0x00,  // call 0x0000C0F0
+        0x84, 0xC0,					   // test al, al
+        0x74, 0x04,					   // je short 0x04
+        0xB0, 0x01,					   // mov al, 0x01
+        0x5D,						   // pop ebp
+        0xC3,						   // ret
+        0xB0, 0x01,					   // mov al, 0x01   <===== Force bypass
+        0x5D,						   // pop ebp
+        0xC3 						   // ret
+    };
+    size_t newSize = sizeof(newBytes) / sizeof(char);
+
+    char* pos = std::search(buffer, buffer + size, searchBytes, searchBytes + searchSize);
+    if (pos != buffer + size) {
+        std::copy(newBytes, newBytes + newSize, pos);
+
+        dllFile.seekp(pos - buffer, std::ios::beg);
+        dllFile.write(newBytes, newSize);
+        std::wcout << L"字节序列已成功替换。" << std::endl;
+    }
+    else {
+        std::wcout << L"未找到指定的字节序列。" << std::endl;
+    }
+
+    delete[] buffer;
+    dllFile.close();
 }
 
-HMODULE WINAPI newLoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD  dwFlags) {
+static HMODULE WINAPI newLoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD  dwFlags) {
     wprintf(L"Load DLL: %s\n", lpLibFileName);
     std::wstring libFileName(lpLibFileName);
     if (libFileName.find(L"appdata\\local\\temp") != std::wstring::npos) {
@@ -52,7 +95,7 @@ HMODULE WINAPI newLoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD  dwF
     return rawLoadLibraryExW(lpLibFileName, hFile, dwFlags);
 }
 
-VOID StartHook() {
+static VOID StartHook() {
     rawLoadLibraryExW = (pLoadLibraryExW)GetProcAddress(GetModuleHandle(L"kernelbase.dll"), "LoadLibraryExW");
 
     DetourRestoreAfterWith();
@@ -62,7 +105,7 @@ VOID StartHook() {
     DetourTransactionCommit();
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+static BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
         /*
